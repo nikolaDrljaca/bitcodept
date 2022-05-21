122d54967c613ee5d37a7ccf90ec3bb332bffeb9
nikolaDrljaca <devnikoladr@gmail.com>
2022-05-21 22:28:47 +0300

    Redesigned code creation dialog to use a normal dialog instead of bottom sheet.

--- app/src/main/java/com/drbrosdev/qrscannerfromlib/util/FragmentStateUtils.kt
+++ app/src/main/java/com/drbrosdev/qrscannerfromlib/util/FragmentStateUtils.kt
@@ -1,5 +1,6 @@
 package com.drbrosdev.qrscannerfromlib.util
 
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -12,9 +13,29 @@ import kotlinx.coroutines.launch
 Grab dependencies here:
     * repeatOnLifeCycle: 'androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha02'
  */
-inline fun <T> Fragment.collectFlow(flow: Flow<T>, crossinline action: ((T) -> Unit)) {
+inline fun <T> Fragment.collectFlow(
+    flow: Flow<T>,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    crossinline action: ((T) -> Unit)
+) {
     viewLifecycleOwner.lifecycleScope.launch {
-        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(lifecycleState) {
+            flow.collect { action(it) }
+        }
+    }
+}
+
+/*
+When working with [DialogFragment] and its subclasses it is recommended to use
+the fragment itself for coroutine related stuff rather than the [viewLifecycleOwner].
+ */
+inline fun <T> DialogFragment.collectFlow(
+    flow: Flow<T>,
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    crossinline action: ((T) -> Unit)
+) {
+    lifecycleScope.launch {
+        lifecycle.repeatOnLifecycle(lifecycleState) {
             flow.collect { action(it) }
         }
     }
